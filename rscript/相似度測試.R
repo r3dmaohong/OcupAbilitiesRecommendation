beca8c75af663c65342f912aca35cf94afe3f8e0
r3dmaohong <r3dmaohong@gmail.com>
2016-05-18 17:49:07 +0800

    add

---
+++ rscript/相似度測試.R
@@ -0,0 +1,37 @@
+rm(list = ls()) #去除工作空間中所有物件
+gc() #記憶體釋放
+path<-"D:\\abc\\wjhong\\projects\\ocup_recommend"
+setwd(path)
+
+options(stringsAsFactors = FALSE)
+
+
+ocup = read.csv(file.choose())
+ocup = ocup[which(ocup$職務X職能指標關聯性=='高'),]
+
+job = unique(ocup$職務小類名稱)
+
+ocup_r = data.frame('職務名稱'=character(),'對應職務名稱'=character(),'相似度'=numeric())
+
+ocup_r[1:length(job)-1,1] = job[1]
+ocup_r[1:length(job)-1,2] = job[2:length(job)]
+
+main = ocup$職能指標小類名稱[which(ocup$職務小類名稱==job[1])]
+
+for(i in 1:nrow(ocup_r)){
+  sub = ocup$職能指標小類名稱[which(ocup$職務小類名稱==ocup_r[i,2])]
+  tmp = 0
+  for(j in 1:length(main)){
+    if(main[j] %in% sub){
+      tmp = tmp + 1
+    }
+  }
+  if(tmp==0){
+    ocup_r[i,3] = 0
+  }else{
+    ocup_r[i,3] = (tmp*2)/(length(main)+length(sub))
+  }
+}
+
+ocup_r = ocup_r[order(-ocup_r[,3]),]
+write.csv(ocup_r,'測試3.csv',row.names=F)
